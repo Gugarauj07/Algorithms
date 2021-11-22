3eada06d855375377be7bbde29a3fa353d349c1b
gugarauj07 <gugaraujo01@gmail.com>
2021-11-22 15:00:38 -0400

    .

---
+++ Beecrowd/1002.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    double area, n, raio;
+    n = 3.14159;
+    scanf("%lf", &raio);
+    area = n * (raio * raio);
+    printf("A=%0.4lf\n", area);
+    return 0;
+}
