45fc192eb1e2d36bb36cf31c83f82e1c3d0e5c54
Gugarauj07 <gugaraujo01@gmail.com>
2022-05-30 22:34:28 -0400

    .

--- Beecrowd/1047.java
+++ Beecrowd/1047.java
@@ -8,17 +8,15 @@ public class Main {
         int min_inicio = ler.nextInt();
         int hora_final = ler.nextInt();
         int min_final = ler.nextInt();
-        int dif;
+        int dif, hora, min;
 
         if (hora_inicio < hora_final) dif = ((hora_final*60) + min_final) - ((hora_inicio*60) + min_inicio);
+        else if (hora_inicio > hora_final) dif = (((hora_final+24)*60) + min_final) - ((hora_inicio*60) + min_inicio);
+        else if (min_inicio < min_final) dif = ((hora_final*60) + min_final) - ((hora_inicio*60) + min_inicio);
         else dif = (((hora_final+24)*60) + min_final) - ((hora_inicio*60) + min_inicio);
-
         
-        
-        int hora = dif/60;
-        int min = dif%60;
-    
-        
+        hora = (int) dif/60;
+        min = (int) dif%60;
 
 		System.out.println(String.format("O JOGO DUROU %d HORA(S) E %d MINUTO(S)", hora, min));
         ler.close();
